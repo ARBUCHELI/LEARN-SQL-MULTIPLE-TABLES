f2a5b36552aed07cb3722db76af12d2ddad83648
ARBUCHELI <radio_seattle@hotmail.com>
2023-02-09 13:47:02 -0500

    creating again orders table

--- multiple_tables.sql
+++ multiple_tables.sql
@@ -3,5 +3,68 @@
     customer_id INT,
     subscription_id INT,
     purchase_date DATE
-)*/
+)
+
+INSERT INTO orders (order_id, customer_id, subscription_id, purchase_date)
+VALUES (1, 3, 2, '01-10-2017');
+
+INSERT INTO orders (order_id, customer_id, subscription_id, purchase_date)
+VALUES (2, 2, 4, '01-9-2017');
+
+INSERT INTO orders (order_id, customer_id, subscription_id, purchase_date)
+VALUES (3, 3, 4, '01-26-2017');
+
+INSERT INTO orders (order_id, customer_id, subscription_id, purchase_date)
+VALUES (4, 9, 9, '01-4-2017');
+
+INSERT INTO orders (order_id, customer_id, subscription_id, purchase_date)
+VALUES (5, 7, 5, '01-25-2017');
+
+INSERT INTO orders (order_id, customer_id, subscription_id, purchase_date)
+VALUES (6, 8, 2, '01-18-2017');
+
+INSERT INTO orders (order_id, customer_id, subscription_id, purchase_date)
+VALUES (7, 5, 8, '01-11-2017');
+
+INSERT INTO orders (order_id, customer_id, subscription_id, purchase_date)
+VALUES (8, 9, 5, '01-26-2017');
+
+INSERT INTO orders (order_id, customer_id, subscription_id, purchase_date)
+VALUES (9, 4, 4, '01-25-2017');
+
+INSERT INTO orders (order_id, customer_id, subscription_id, purchase_date)
+VALUES (10, 1, 7, '01-26-2017');
+
+INSERT INTO orders (order_id, customer_id, subscription_id, purchase_date)
+VALUES (11, 5, 4, '01-7-2017');
+
+INSERT INTO orders (order_id, customer_id, subscription_id, purchase_date)
+VALUES (12, 3, 2, '01-21-2017');
+
+INSERT INTO orders (order_id, customer_id, subscription_id, purchase_date)
+VALUES (13, 3, 5, '01-3-2017');
+
+INSERT INTO orders (order_id, customer_id, subscription_id, purchase_date)
+VALUES (14, 6, 5, '01-22-2017');
+
+INSERT INTO orders (order_id, customer_id, subscription_id, purchase_date)
+VALUES (15, 1, 2, '01-6-2017');
+
+INSERT INTO orders (order_id, customer_id, subscription_id, purchase_date)
+VALUES (16, 1, 2, '01-11-2017');
+
+INSERT INTO orders (order_id, customer_id, subscription_id, purchase_date)
+VALUES (17, 3, 6, '01-17-2017');
+
+INSERT INTO orders (order_id, customer_id, subscription_id, purchase_date)
+VALUES (18, 3, 8, '01-29-2017');
+
+INSERT INTO orders (order_id, customer_id, subscription_id, purchase_date)
+VALUES (19, 4, 9, '01-3-2017');
+
+INSERT INTO orders (order_id, customer_id, subscription_id, purchase_date)
+VALUES (20, 1, 7, '01-24-2017');*/
+
+
+
 
