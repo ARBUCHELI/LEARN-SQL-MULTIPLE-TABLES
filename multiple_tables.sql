56b9911fe0da11de3a538a1a424e9832a17e2d95
ARBUCHELI <radio_seattle@hotmail.com>
2023-02-12 11:07:13 -0500

    adding join, combining tables with sql

--- multiple_tables.sql
+++ multiple_tables.sql
@@ -156,5 +156,14 @@ SELECT *
 FROM customers
 LIMIT 5;
 
+SELECT *
+FROM orders
+JOIN subscriptions
+  ON orders.subscription_id = subscriptions.subscription_id;
 
+SELECT *
+FROM orders
+JOIN subscriptions
+  ON orders.subscription_id = subscriptions.subscription_id
+  WHERE subscriptions.description = 'Fashion Magazine';
 
